61a7cef24c984a23595aa5a7d3ef7e3d522f41d5
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2020-09-30 20:00:03 -0300

    Structue API

--- react-api-mongo/src/App.js
+++ react-api-mongo/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from './api';
 
 
-function App() {
-  return (
-    <div >
-      <h1> React seco !</h1>
-    </div>
-  );
+class App extends Component {
+  // Definindo o estado
+  state = {
+    artigos: [],
+  }
+
+  async componentDidMount() {
+    // definindo a var que conterá o retorno da API
+    const response = await api.get('');
+
+    this.setState({ artigos: response.data });
+    console.log(response.data);
+  }
+
+  render() {
+    return
+    (
+      <div>
+        <strong> <h1>API React.js|MongoDB </h1></strong>
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ react-api-mongo/src/api.js
@@ -0,0 +1,9 @@
+// Configurando o acesso a api com axios
+import axios from 'axios';
+
+// Criando URL base de acesso
+const api = axios.create({
+    baseURL: 'http://localhost:8080/api'
+});
+
+export default api;
